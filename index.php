a5b2d49241f5443395f03fc14b93ee6cf2e5e35c
indunilwijesundara <indunilwijesundara@gmail.com>
2023-09-03 09:31:13 +0530

    updated

--- index.php
+++ index.php
@@ -1,16 +1,28 @@
-<?php include 'layouts/session.php';
+<?php
+// Include necessary files and database connection
+require_once "layouts/config.php";
+include 'layouts/session.php';
+include 'layouts/head-main.php';
+
+// Fetch data from the database (example)
+$query = "SELECT * FROM invoice_commercial";
+$result = mysqli_query($link, $query);
+
+// Get the row count
+$rowCount = mysqli_num_rows($result);
+
+
 ?>
-<?php include 'layouts/head-main.php'; ?>
 
 <head>
 
-    <title><?php echo $language["Dashboard"]; ?> | Flotilla - Admin & Dashboard Template</title>
+    <title><?php echo $language["Dashboard"]; ?> | Flotilla - Admin & Dashboard </title>
 
     <?php include 'layouts/head.php'; ?>
     <link href="assets/libs/admin-resources/jquery.vectormap/jquery-jvectormap-1.2.2.css" rel="stylesheet"
         type="text/css" />
     <?php include 'layouts/head-style.php'; ?>
-
+    <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
 </head>
 
 <?php include 'layouts/body.php'; ?>
@@ -36,7 +48,11 @@
                 <?php include 'layouts/breadcrumb.php'; ?>
                 <!-- end page title -->
 
+
+
                 <div class="row">
+
+
                     <div class="col-xl-3 col-md-6">
                         <!-- card -->
                         <div class="card card-h-100">
@@ -44,43 +60,16 @@
                             <div class="card-body">
                                 <div class="d-flex align-items-center">
                                     <div class="flex-grow-1">
-                                        <span class="text-muted mb-3 lh-1 d-block text-truncate">Total Sales</span>
+                                        <span class="text-muted mb-3 lh-1 d-block text-truncate">Total Exports</span>
                                         <h4 class="mb-3">
-                                            $<span class="counter-value" data-target="354.5">0</span>k
+
+                                            <?php echo $rowCount; ?>
                                         </h4>
-                                        <div class="text-nowrap">
-                                            <span class="badge bg-soft-success text-success">+$20.9k</span>
-                                            <span class="ms-1 text-muted font-size-13">Since last week</span>
-                                        </div>
+
                                     </div>
 
                                     <div class="flex-shrink-0 text-end dash-widget">
                                         <div id="mini-chart1" data-colors='["#1c84ee", "#33c38e"]' class="apex-charts">
-                                        </div>
-                                    </div>
-                                </div>
-                            </div><!-- end card body -->
-                        </div><!-- end card -->
-                    </div><!-- end col -->
-
-                    <div class="col-xl-3 col-md-6">
-                        <!-- card -->
-                        <div class="card card-h-100">
-                            <!-- card body -->
-                            <div class="card-body">
-                                <div class="d-flex align-items-center">
-                                    <div class="flex-grow-1">
-                                        <span class="text-muted mb-3 lh-1 d-block text-truncate">Total Items</span>
-                                        <h4 class="mb-3">
-                                            <span class="counter-value" data-target="1256">0</span>
-                                        </h4>
-                                        <div class="text-nowrap">
-                                            <span class="badge bg-soft-danger text-danger">-29 Trades</span>
-                                            <span class="ms-1 text-muted font-size-13">Since last week</span>
-                                        </div>
-                                    </div>
-                                    <div class="flex-shrink-0 text-end dash-widget">
-                                        <div id="mini-chart2" data-colors='["#1c84ee", "#33c38e"]' class="apex-charts">
                                         </div>
                                     </div>
                                 </div>
@@ -142,143 +131,18 @@
                 <div class="row">
                     <div class="col-xl-8">
                         <!-- card -->
-                        <div class="card">
+                        <div class="card card-h-100">
                             <!-- card body -->
-                            <div class="card-body">
-                                <div class="d-flex flex-wrap align-items-center mb-4">
-                                    <h5 class="card-title me-2">Market Overview</h5>
-                                    <div class="ms-auto">
-                                        <div>
-                                            <button type="button" class="btn btn-soft-primary btn-sm">
-                                                ALL
-                                            </button>
-                                            <button type="button" class="btn btn-soft-secondary btn-sm">
-                                                1M
-                                            </button>
-                                            <button type="button" class="btn btn-soft-secondary btn-sm">
-                                                6M
-                                            </button>
-                                            <button type="button" class="btn btn-soft-secondary btn-sm active">
-                                                1Y
-                                            </button>
-                                        </div>
-                                    </div>
-                                </div>
+                            <div class="card-body ">
 
-                                <div class="row align-items-center">
-                                    <div class="col-xl-8">
-                                        <div>
-                                            <div id="market-overview" data-colors='["#1c84ee", "#33c38e"]'
-                                                class="apex-charts"></div>
-                                        </div>
-                                    </div>
-                                    <div class="col-xl-4">
-                                        <div class="p-4">
-                                            <div class="mt-0">
-                                                <div class="d-flex align-items-center">
-                                                    <div class="avatar-sm m-auto">
-                                                        <span
-                                                            class="avatar-title rounded-circle bg-light text-dark font-size-13">
-                                                            1
-                                                        </span>
-                                                    </div>
-                                                    <div class="flex-grow-1 ms-3">
-                                                        <span class="font-size-14">Mobile Phones</span>
-                                                    </div>
+                                <div id="chartContainer"></div>
 
-                                                    <div class="flex-shrink-0">
-                                                        <span
-                                                            class="badge rounded-pill badge-soft-success font-size-12 fw-medium">+5.4%</span>
-                                                    </div>
-                                                </div>
-                                            </div>
 
-                                            <div class="mt-3">
-                                                <div class="d-flex align-items-center">
-                                                    <div class="avatar-sm m-auto">
-                                                        <span
-                                                            class="avatar-title rounded-circle bg-light text-dark font-size-13">
-                                                            2
-                                                        </span>
-                                                    </div>
-                                                    <div class="flex-grow-1 ms-3">
-                                                        <span class="font-size-14">Smart Watch</span>
-                                                    </div>
-
-                                                    <div class="flex-shrink-0">
-                                                        <span
-                                                            class="badge rounded-pill badge-soft-success font-size-12 fw-medium">+6.8%</span>
-                                                    </div>
-                                                </div>
-                                            </div>
-
-                                            <div class="mt-3">
-                                                <div class="d-flex align-items-center">
-                                                    <div class="avatar-sm m-auto">
-                                                        <span
-                                                            class="avatar-title rounded-circle bg-light text-dark font-size-13">
-                                                            3
-                                                        </span>
-                                                    </div>
-                                                    <div class="flex-grow-1 ms-3">
-                                                        <span class="font-size-14">Protable Acoustics</span>
-                                                    </div>
-
-                                                    <div class="flex-shrink-0">
-                                                        <span
-                                                            class="badge rounded-pill badge-soft-danger font-size-12 fw-medium">-4.9%</span>
-                                                    </div>
-                                                </div>
-                                            </div>
-
-                                            <div class="mt-3">
-                                                <div class="d-flex align-items-center">
-                                                    <div class="avatar-sm m-auto">
-                                                        <span
-                                                            class="avatar-title rounded-circle bg-light text-dark font-size-13">
-                                                            4
-                                                        </span>
-                                                    </div>
-                                                    <div class="flex-grow-1 ms-3">
-                                                        <span class="font-size-14">Smart Speakers</span>
-                                                    </div>
-
-                                                    <div class="flex-shrink-0">
-                                                        <span
-                                                            class="badge rounded-pill badge-soft-success font-size-12 fw-medium">+3.5%</span>
-                                                    </div>
-                                                </div>
-                                            </div>
-
-                                            <div class="mt-3">
-                                                <div class="d-flex align-items-center">
-                                                    <div class="avatar-sm m-auto">
-                                                        <span
-                                                            class="avatar-title rounded-circle bg-light text-dark font-size-13">
-                                                            5
-                                                        </span>
-                                                    </div>
-                                                    <div class="flex-grow-1 ms-3">
-                                                        <span class="font-size-14">Camcorders</span>
-                                                    </div>
-
-                                                    <div class="flex-shrink-0">
-                                                        <span
-                                                            class="badge rounded-pill badge-soft-danger font-size-12 fw-medium">-0.3%</span>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="mt-4 pt-2">
-                                                <a href="" class="btn btn-primary w-100">See All Balances <i
-                                                        class="mdi mdi-arrow-right ms-1"></i></a>
-                                            </div>
-
-                                        </div>
-                                    </div>
-                                </div>
                             </div>
-                            <!-- end card -->
+                            <!-- end card body -->
                         </div>
+
+
                         <!-- end col -->
                     </div>
                     <!-- end row-->
@@ -342,11 +206,48 @@
                 </div>
                 <!-- end row-->
 
-
             </div>
             <!-- end col -->
 
+            <script>
+            var options = {
+                series: [{
+                    name: "Desktops",
+                    data: [10, 41, 35, 51, 49, 62, 69, 91, 148]
+                }],
+                chart: {
+                    height: 450,
+                    width: 680,
+                    type: 'line',
+                    zoom: {
+                        enabled: false
+                    }
+                },
+                dataLabels: {
+                    enabled: false
+                },
+                stroke: {
+                    curve: 'straight'
+                },
+                title: {
+                    text: 'Product Trends by Month',
+                    align: 'left'
+                },
+                grid: {
+                    row: {
+                        colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
+                        opacity: 0.5
+                    },
+                },
+                xaxis: {
+                    categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep'],
+                }
+            };
 
+            // Render the chart inside the "chartContainer" div
+            var chart = new ApexCharts(document.querySelector("#chartContainer"), options);
+            chart.render();
+            </script>
         </div>
         <!-- end col -->
 
@@ -365,7 +266,6 @@
 
 </div>
 <!-- END layout-wrapper -->
-
 <?php include 'layouts/right-sidebar.php'; ?>
 
 <?php include 'layouts/vendor-scripts.php'; ?>
@@ -377,6 +277,7 @@
 <script src="assets/libs/admin-resources/jquery.vectormap/maps/jquery-jvectormap-world-mill-en.js"></script>
 <!-- dashboard init -->
 <script src="assets/js/pages/dashboard.init.js"></script>
+
 
 
 </body>
